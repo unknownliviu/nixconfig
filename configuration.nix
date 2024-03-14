263bbc23cd37e4cefd40ba8da437e33470a39853
Liviu Maria <marialiviuvalentin@gmail.com>
2024-03-14 12:31:53 +0200

    latest nix config

--- configuration.nix
+++ configuration.nix
@@ -33,6 +33,10 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
+  systemd.extraConfig = ''
+    DefaultTimeoutStopSec=10s
+  '';
+
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
