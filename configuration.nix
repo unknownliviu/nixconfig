e4dbe9d499afed6e3c74f814c59f76713e8200b2
Liviu Maria <marialiviuvalentin@gmail.com>
2024-03-17 08:28:22 +0200

    latest nix config

--- configuration.nix
+++ configuration.nix
@@ -181,6 +181,7 @@
   programs.vim = {
     defaultEditor = true;
   };
+  programs.steam.enable = true;
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
