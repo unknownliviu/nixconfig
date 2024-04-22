ca85207ab26cab416ea94d085953152a53c5132e
Liviu Maria <marialiviuvalentin@gmail.com>
2024-04-22 14:07:57 +0300

    latest nix config

--- configuration.nix
+++ configuration.nix
@@ -180,6 +180,7 @@
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
     mdadm
+    kmail
     killall
     jellyfin
     jellyfin-web
